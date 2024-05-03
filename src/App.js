e60b77727a289be917b14bed6c4ff062a25be265
kas-neu <153776294+kas-neu@users.noreply.github.com>
2024-05-03 22:29:29 +0200

    added footer section with links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ const App = () => {
     <div className="App">
       {" "}
       <h1>Weather App</h1>
+      <footer>
+        This project was created by{" "}
+        <a href="https://github.com/kas-neu" target="_blank">
+          Kasia Neugebauer
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a href="https://github.com/kas-neu/weather-app-react" target="_blank">
+          Github
+        </a>
+        .
+      </footer>
     </div>
   );
 };
