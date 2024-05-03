5fc0d8b7d2130d6ab5e7797c09716b6806b8af9a
kas-neu <153776294+kas-neu@users.noreply.github.com>
2024-05-03 22:34:11 +0200

    resolved link issue in the footer

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ const App = () => {
       {" "}
       <h1>Weather App</h1>
       <footer>
-        This project was created by{" "}
-        <a href="https://github.com/kas-neu" target="_blank">
-          Kasia Neugebauer
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a href="https://github.com/kas-neu/weather-app-react" target="_blank">
-          Github
-        </a>
-        .
+        <p>
+          {" "}
+          This project was created by{" "}
+          <a href="https://github.com/kas-neu" target="_blank" rel="noreferrer">
+            Kasia Neugebauer
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/kas-neu/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          .
+        </p>
       </footer>
     </div>
   );
