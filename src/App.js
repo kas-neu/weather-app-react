1121a3184ff08377219592a8cfcc606d57068478
kas-neu <153776294+kas-neu@users.noreply.github.com>
2024-05-05 09:14:35 +0200

    fixed more bugs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <div class="container">
+      <div className="container">
         <Weather defaultCity="London" />
         <footer>
           <p>
